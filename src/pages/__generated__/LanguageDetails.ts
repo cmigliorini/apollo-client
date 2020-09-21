82c4de282856d0f0ecccfb219c9749cbd2052597
Christophe Migliorini <christophe@migliorini.fr>
2020-09-22 01:00:29 +0200

    add all  languages and language detail

---
+++ src/pages/__generated__/LanguageDetails.ts
@@ -0,0 +1,23 @@
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL query operation: LanguageDetails
+// ====================================================
+
+export interface LanguageDetails_language {
+  __typename: "Language";
+  id: string;
+  name: string;
+  description: string | null;
+}
+
+export interface LanguageDetails {
+  language: LanguageDetails_language | null;
+}
+
+export interface LanguageDetailsVariables {
+  languageId: string;
+}
